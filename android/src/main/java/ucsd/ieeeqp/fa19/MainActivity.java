6896586d4ac0dc4205c8583fe215fdc3c7bd8215
Chi Chow <kyroschow@gmail.com>
2019-10-19 22:20:59 -0700

    Update .gitignore and optimize imports

--- android/src/main/java/ucsd/ieeeqp/fa19/MainActivity.java
+++ android/src/main/java/ucsd/ieeeqp/fa19/MainActivity.java
@@ -1,9 +1,7 @@
 package ucsd.ieeeqp.fa19;
 
-import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
