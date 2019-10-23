745318c9b668530ebca4f03f94c3491e06e87e8e
eekaireb <eekaireb@ucsd.edu>
2019-10-22 19:26:25 -0700

    changed textView to say Hello World!

--- android/src/main/java/ucsd/ieeeqp/fa19/MainActivity.java
+++ android/src/main/java/ucsd/ieeeqp/fa19/MainActivity.java
@@ -1,6 +1,7 @@
 package ucsd.ieeeqp.fa19;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView_main_greeting);
+        textView.setText("Hello World");
     }
 }
