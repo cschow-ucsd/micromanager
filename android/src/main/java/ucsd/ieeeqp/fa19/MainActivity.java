9887a0450bc4249766586ef86133c940d0ef6b76
Chi Chow <kyroschow@gmail.com>
2019-10-22 19:30:26 -0700

    fixed upper case

--- android/src/main/java/ucsd/ieeeqp/fa19/MainActivity.java
+++ android/src/main/java/ucsd/ieeeqp/fa19/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.textView_main_greeting);
+        TextView textView = findViewById(R.id.textview_main_greeting);
         textView.setText("Hello World");
 
     }
