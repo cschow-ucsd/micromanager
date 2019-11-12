4779a28acced7bd241e058f27ab354813b1499ad
Chi Chow <kyroschow@gmail.com>
2019-11-12 11:54:03 -0800

    ID in users is equal to subject

--- backend/src/main/kotlin/exposed/dsl/MmUsers.kt
+++ backend/src/main/kotlin/exposed/dsl/MmUsers.kt
@@ -7,7 +7,5 @@ import org.jetbrains.exposed.sql.Column
 object MmUsers : IdTable<String>() {
     override val id: Column<EntityID<String>> = varchar("subject", 50).primaryKey().entityId()
     val email = varchar("email", 50)
-    val refreshToken = varchar("refresh_token", 1500).nullable()
-    val subject: Column<EntityID<String>>
-        get() = id
+    val refreshToken = text("refresh_token").nullable()
 }
