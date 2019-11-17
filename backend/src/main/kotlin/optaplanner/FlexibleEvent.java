7fb83712083b930864b3951f73de4594d2bb163b
yuyeon <li.alan180@gmail.com>
2019-11-16 19:03:26 -0800

    bua bua bua

---
+++ backend/src/main/kotlin/optaplanner/FlexibleEvent.java
@@ -0,0 +1,21 @@
+package optaplanner;
+
+import org.optaplanner.core.api.domain.entity.PlanningEntity;
+import org.optaplanner.core.api.domain.variable.PlanningVariable;
+
+@PlanningEntity
+public class FlexibleEvent {
+    private String name;
+    private Integer startTime, endTime, duration;
+    private String type;
+    public static final String SOME_TYPE = ""; // TODO: kinds constants
+
+    @PlanningVariable(valueRangeProviderRefs = {"availableStartTimes"})
+    public Integer getStartTime() {
+        return startTime;
+    }
+
+    public Integer getDuration(){
+        return duration;
+    }
+}
