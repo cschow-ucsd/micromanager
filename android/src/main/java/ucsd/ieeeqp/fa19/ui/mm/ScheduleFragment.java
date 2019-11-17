d3f5896542b92df612d884b005397d560c9e62fc
eekaireb <eekaireb@ucsd.edu>
2019-11-16 18:47:59 -0800

    made a button

--- android/src/main/java/ucsd/ieeeqp/fa19/ui/mm/ScheduleFragment.java
+++ android/src/main/java/ucsd/ieeeqp/fa19/ui/mm/ScheduleFragment.java
@@ -9,7 +9,17 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import ucsd.ieeeqp.fa19.R;
 
+import java.util.ArrayList;
+
 public class ScheduleFragment extends Fragment {
+    private ArrayList<Event> schedule;
+
+    public ScheduleFragment()
+    {
+        schedule = new ArrayList<Event>();
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
