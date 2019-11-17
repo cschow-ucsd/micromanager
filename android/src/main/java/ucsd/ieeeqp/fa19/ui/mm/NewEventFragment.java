603a7c85d3aec07b278c999007a2ea26b88e640f
lad01 <dercolelaurence@gmail.com>
2019-11-16 19:01:31 -0800

    added 5 "edit text" boxes for user input

---
+++ android/src/main/java/ucsd/ieeeqp/fa19/ui/mm/NewEventFragment.java
@@ -0,0 +1,19 @@
+package ucsd.ieeeqp.fa19.ui.mm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import ucsd.ieeeqp.fa19.R;
+
+public class NewEventFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_event, container, false);
+    }
+}
+
