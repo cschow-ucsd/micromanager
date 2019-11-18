d8bc60652ff8da91186ca51ce9f7408675333dcc
Chi Chow <kyroschow@gmail.com>
2019-11-17 23:34:28 -0800

    Remove unused loadConfig function

--- backend/src/main/kotlin/MmHoconConfig.kt
+++ backend/src/main/kotlin/MmHoconConfig.kt
@@ -14,11 +14,3 @@ object MmHoconConfig : HoconApplicationConfig(ConfigFactory.load()) {
     val mmClientSecret = this["mm.clientSecret"]
     val mmTransformerHex = this["mm.transformerHex"]
 }
-
-/**
- * Loads application configuration from .conf files in resources
- * When cloned
- */
-//private fun loadMmConfig(): Config =
-//        ConfigFactory.parseResources("POSTGRES_MM_URL = \"jdbc:postgresql://localhost:5432/micromanager\"\nPOSTGRES_USER = \"postgres\"\nPOSTSGRES_PASSWORD = \"kyroschow\"\n\nMM_CLIENT_SECRET = \"uYFVhIXwtaMsrrvzWaptTaRj\"\nMM_TRANSFORMER_HEX = \"da4321dded2b27ce1c47baa18f91a1\"\n")
-//        .withFallback(ConfigFactory.parseResources("application.conf"))
