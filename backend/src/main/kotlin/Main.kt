c107cbce52f7e311b1fe9f25e33b99faae61ac40
Chi Chow <kyroschow@gmail.com>
2019-11-17 23:30:14 -0800

    Add reference.conf to easily add secret keys

--- backend/src/main/kotlin/Main.kt
+++ backend/src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
+import com.typesafe.config.ConfigException
 import io.ktor.server.netty.EngineMain
+import io.ktor.util.KtorExperimentalAPI
 
 /**
  * Entry point that starts the server.
  * Uses [EngineMain] with application.conf to start Netty server.
  */
-fun main(args: Array<String>): Unit = EngineMain.main(args)
+@KtorExperimentalAPI
+fun main(args: Array<String>) {
+    try {
+        EngineMain.main(args)
+    } catch (e: ConfigException.UnresolvedSubstitution) {
+        throw RuntimeException("Unresolved substitution in config. Missing reference.conf in resources?", e)
+    }
+}
