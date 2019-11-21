5a44c172ebb509931c567a7e2c875c5a7d1c2da1
yuyeon <li.alan180@gmail.com>
2019-11-20 19:53:27 -0800

    yeet

--- backend/src/main/kotlin/Main.kt
+++ backend/src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import com.typesafe.config.ConfigException
 import io.ktor.server.netty.EngineMain
 import io.ktor.util.KtorExperimentalAPI
+import optaplanner.EventSchedule
+import org.optaplanner.core.api.solver.SolverFactory
 
 /**
  * Entry point that starts the server.
@@ -8,9 +10,20 @@ import io.ktor.util.KtorExperimentalAPI
  */
 @KtorExperimentalAPI
 fun main(args: Array<String>) {
-    try {
-        EngineMain.main(args)
-    } catch (e: ConfigException.UnresolvedSubstitution) {
-        throw RuntimeException("Unresolved substitution in config. Missing reference.conf in resources?", e)
-    }
+    solverTest()
+//    try {
+//        EngineMain.main(args)
+//    } catch (e: ConfigException.UnresolvedSubstitution) {
+//        throw RuntimeException("Unresolved substitution in config. Missing reference.conf in resources?", e)
+//    }
+}
+
+private fun solverTest() {
+    val solverFactory = SolverFactory
+            .createFromXmlResource<EventSchedule>("event_schedule_solver_configuration.xml")
+    val solver = solverFactory.buildSolver()
+
+    val unsolvedEventSchedule = EventSchedule()
+
+    val solvedEventSchedule = solver.solve(unsolvedEventSchedule) as EventSchedule
 }
