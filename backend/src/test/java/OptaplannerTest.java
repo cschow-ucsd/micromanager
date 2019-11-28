a190c5ae3b325940baf4b1b37c463d06f1fabc5d
yuyeon <li.alan180@gmail.com>
2019-11-27 22:27:06 -0800

    it looks like a lion

--- backend/src/test/java/OptaplannerTest.java
+++ backend/src/test/java/OptaplannerTest.java
@@ -15,15 +15,15 @@ public class OptaplannerTest {
     @Test
     public void optaplannerTest() {
         ArrayList<BaseFixedEvent> a = new ArrayList<>();
-        a.add(new PlannedFixedEvent("Sleep", 0, 0, 0, 420));
-        a.add(new PlannedFixedEvent("IEEE Workathon", 0, 0, 840, 960));
-        a.add(new PlannedFixedEvent("CSE 20", 0, 0, 540, 590));
         ArrayList<PlanningFlexibleEvent> b = new ArrayList<>();
-        b.add(new PlanningFlexibleEvent("Breakfast", "Breakfast", 0, 0, 20));
-        b.add(new PlanningFlexibleEvent("Lunch", "Lunch", 0, 0, 30));
-        b.add(new PlanningFlexibleEvent("Dinner", "Dinner", 0 ,0, 30));
-        b.add(new PlanningFlexibleEvent("CSE 20 HW", "HW", 0, 0, 180));
-        b.add(new PlanningFlexibleEvent("Work on QP Project", "Club", 0, 0, 180));
+       a.add(new PlannedFixedEvent("Sleep", 0, 420, 0, 0));
+        a.add(new PlannedFixedEvent("IEEE Workathon", 840, 960, 0, 0));
+        a.add(new PlannedFixedEvent("CSE 20", 540, 600, 0, 0));
+        b.add(new PlanningFlexibleEvent("Breakfast", "Breakfast", 40, 0, 0));
+        b.add(new PlanningFlexibleEvent("Lunch", "Lunch", 70, 0, 0));
+        b.add(new PlanningFlexibleEvent("Dinner", "Dinner", 60 ,0, 0));
+        b.add(new PlanningFlexibleEvent("CSE 20 HW", "HW", 50, 0, 0));
+        b.add(new PlanningFlexibleEvent("Work on QP Project", "Club", 120, 0, 0));
         EventSchedule e = new EventSchedule(a, b,
                 new BaseUserPreferences(460, 480, 600, 630, 960, 1000, 60, 60));
         SolverFactory<EventSchedule> s = SolverFactory.createFromXmlResource("event_schedule_solver_configuration.xml");
